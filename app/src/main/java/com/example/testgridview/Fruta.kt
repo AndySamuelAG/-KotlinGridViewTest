de4668e1c041152950219bcbdb52a6a4634e4965
Andy Alvarado <AndyAlvarado1997@gmail.com>
2020-02-14 16:48:50 -0500

    Init

---
+++ app/src/main/java/com/example/testgridview/Fruta.kt
@@ -0,0 +1,13 @@
+package com.example.testgridview
+
+class Fruta(nombre:String, imagen:Int, valor:String) {
+    var nombre:String = ""
+    var imagen:Int = 0
+    var valor:String = ""
+
+    init {
+        this.nombre = nombre
+        this.imagen = imagen
+        this.valor = valor
+    }
+}
